1d08751cb0bbcc153b5be19826547adf0089b9d3
deepak <code.deepak9899sharma@gmail.com>
2026-02-02 12:11:52 +0530

    1. Variable and Data Types

---
+++ PROGRAMMING/PYTHON/1. Variable and Data Types/main.py
@@ -0,0 +1,56 @@
+# comments (ctrl + /) : describe
+
+# 1. What is variable(data containers) ??
+# it is the data container : it is the name given to address block which contains the data
+
+# steps to create a variable ---> [ declare + assign ]
+num1 = 287
+num2 = 10
+
+
+
+# 2. Data types?
+"""
+number : int , float
+text : string
+Boolean : true , false
+Empty : None
+series of related data : List[] 
+series of related data with keyName : dict{}
+"""
+
+
+age = 22
+name = "Deepak Sharma"
+isMarried = False
+visa = None
+
+# skills1 = "HTML" 
+# skills2 = "CSS" 
+# skills3 = "JS" 
+skills = ["HTML", "CSS", "JS" , "Python" ] # index=0
+
+mySelf = {
+    "name":"Deepak Sharma",
+    "age":22,
+    "isMarried":False,
+    "visa":None,
+    "skills": ["HTML", "CSS", "JS" , "Python" ]
+}
+
+
+# To check data types of Variable
+# print(type(age))
+
+
+# 3. Naming of a Variable
+"""
+>> this is word
+camel Case : thisIsWord
+snake case : this_is_word
+
+
+*** No space between variable name(identifiers)
+"""
+
+
