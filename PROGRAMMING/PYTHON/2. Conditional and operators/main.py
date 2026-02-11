0ef6d0e347d9b6fa48ede43ef6a8f0ed2b40944d
deepak <code.deepak9899sharma@gmail.com>
2026-02-11 12:08:10 +0530

    Operators(symbol used to do different operation on values)

---
+++ PROGRAMMING/PYTHON/2. Conditional and operators/main.py
@@ -0,0 +1,60 @@
+# User input
+# type casting
+# calculator
+
+
+num1 = int(input("Enter your first Number : "))
+operation = input("Choose your operations (+,-,*,/) : ")
+num2 = int(input("Enter your second Number : "))
+
+
+# addition output
+# print(type(num1))
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+
+
+# if(operation=="+"):
+#     print(num1+num2)
+# else:
+#     print("Something went wrong")
+
+
+
+# if(operation=="+"):
+#     print(num1+num2)
+
+# if(operation=="-"):
+#     print(num1-num2)
+
+# if(operation=="*"):
+#     print(num1*num2)
+
+# if(operation=="/"):
+#     print(num1/num2)
+
+# conditional statements
+if(operation=="+"):
+    print(num1+num2)
+elif(operation=="-"):
+    print(num1-num2)
+elif(operation=="*"):
+    print(num1*num2)
+elif(operation=="/"):
+    print(num1/num2)
+else:
+    print("Something went wrong")
+
+
+# -----------------Topics
+# int , string , float , boolean
+
+# -> check type of data
+
+
+# Operators(symbol used to do different operation on values)
+# 1. Mathematical opeartors (+,-,*...)
+# 2. if , if-else , if-elif-else : conditional statement
+# 3. type casting
