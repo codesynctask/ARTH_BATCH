9e0efd15ef5d6cce54ea57687422bc16f22e70a2
deepak <code.deepak9899sharma@gmail.com>
2026-02-18 15:42:48 +0530

    PYTHON: Data type and varaible

--- DA/PYTHON/1.Variables and Data Types in Python/main.py
+++ DA/PYTHON/1.Variables and Data Types in Python/main.py
@@ -1,2 +1,35 @@
-print("Hello World")
+# 1. Variable(data)
 
+# 1.1 declare variable
+"""
+Variable Name
+Variable data
+Variable Data type
+"""
+
+name = "Deepak" # string
+age = 22 # int
+marks_of_12th = 63.5 # float
+
+hobbies = ["Coding","music","drwaing"] # list[] : [str , str , str]
+emotions = ("Happiness","Sadness","Anger") # tuple()
+mySelf = {
+    "name":name,
+    "age" : age,
+    "hobbies" : hobbies,
+    "emotions" : emotions
+} # dict{key:value} : combinations of key and value pair
+assests = {"laptop","mobile","mobile"}
+
+
+
+# Output
+# print("Hello my name is",name)
+
+
+# Checking data types (type)
+# print(type(hobbies))
+
+
+# Accessing value of these varaible
+print( hobbies[3] )
