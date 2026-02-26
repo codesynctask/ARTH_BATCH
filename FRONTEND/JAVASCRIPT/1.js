e1256f7d7cf4201509dd551d4b88a3feea13f6fb
deepak <code.deepak9899sharma@gmail.com>
2026-02-26 13:43:33 +0530

    JAVASCRIPT: 1.variable and data types:

---
+++ FRONTEND/JAVASCRIPT/1.js
@@ -0,0 +1,46 @@
+// 1. Variables and Data Types
+
+// ===> Creating Variables (DECLARE) : let , var❌ , const
+
+const MY_NAME = "John Doe" // declare + initialize
+let age = 22
+
+let isStudent = true
+
+const PI = 22/7
+// console.log(age)
+// ====> keyword used to declare the variable : {let}
+// name of variable : MY_NAME
+// type of variable : string
+// value of variable : "John Doe"
+
+
+
+// ====> Output of variable
+// console.log(MY_NAME)
+// console.log("My name is",MY_NAME)
+// console.log(`My name is ${MY_NAME} and my age is ${age}`)
+
+
+// ====> update 
+// MY_NAME = "Deepak Sharma"  // constant assignment error
+// age = 23
+
+let carBrand
+let petrolTank = null
+
+// ====> D/W null and undefined
+carBrand = "BMW"
+console.log(isStudent)
+
+
+// ====> Checking data types
+console.log(typeof(age))
+
+// practice :
+    // ALWAYS DECLARE VARIABLE WITH "CONST"
+
+
+
+
+
