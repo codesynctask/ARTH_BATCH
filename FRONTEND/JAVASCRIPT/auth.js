3bce5d5fdfa2a1de172ce48c7a4641a2d1171958
deepak <code.deepak9899sharma@gmail.com>
2026-04-15 23:16:59 +0530

    FRONTENT/JS : working with adv data stucture with Authentication system

--- FRONTEND/JAVASCRIPT/auth.js
+++ FRONTEND/JAVASCRIPT/auth.js
@@ -18,12 +18,13 @@ Auth
 - deactive - D
 */
 
-const userData = ["rahul","osaka","deepak","gayatri"] //array
+// const userData = [] //array
+
 
 
 // C : at last
 // userData.unshift("Random User")//first
-userData.push("sanskar") //last
+// userData.push("sanskar") //last
 
 // R : find
 // reading all users
@@ -42,9 +43,11 @@ userData.push("sanskar") //last
 // userData[0] = "Updated user"
 
 // D : find and delete
-// delete userData[0] ❌
+// delete userData[0] 
 // userData.pop() // last elm delete
-console.log(userData)
+
+
+
 
 
 
@@ -52,19 +55,87 @@ console.log(userData)
 
 
 // find user with name "deepak" and read
-userData.forEach(function(user,i){
-    if(user==="deepak"){
-        // console.log("Hello Admin")
-        // userData[i] = "ok"
-    }else{
-        console.log("users")
-    }
-})
+// userData.forEach(function(user,i){
+//     if(user==="deepak"){
+//         // console.log("Hello Admin")
+//         // userData[i] = "ok"
+//     }else{
+//         console.log("users")
+//     }
+// })
 
-console.log(userData)
+// console.log(userData)
 
 // array methods
 
 
+const userData = [] // array of objects
+
+// CREATE NEW USER : function
+
+// function declaration + definition
+// parameter : variable for functions
+
+function createUser(name, age , gender , country="IND" , role="user") {
+    // user schema ready only
+    if(!name || !age || !gender){
+        console.log("error")
+        return
+    }
+
+    if(!(name.length>=3)){
+        // name lenght validation
+        console.log("name is not perfect")
+        return
+    }
+    if(!(Number(age)>=12 && Number(age)<=90)){
+        // age value validation
+        console.log("age is not perfect")
+        return
+    }
 
 
+    const newUserSchema = {
+        "id": userData.length + 1,
+        "name": name,
+        "age": age,
+        "gender": gender,
+        "country":country,
+        "role": role
+    }
+
+    // adding new user to userData[]
+    userData.push(newUserSchema)
+}
+
+
+// call to function
+// passign value to function variable(parameter) ---> passing argument
+// createUser("xyz",26,"male")
+// createUser("abc",28,"male")
+createUser("John Doe",22,"male")
+createUser("Kajal",28,"female")
+createUser("Khushi",21,"female")
+// console.log(userData) //reading all user data
+// SIGNUP of user
+
+
+
+function findUserById(id) {
+    let foundUser = null;
+    if(!id){
+        console.log("Id is missing")
+        return
+    }
+
+    userData.forEach(function (user) {
+        if(user["id"]==id){
+            foundUser = user
+        }
+    })
+
+    return foundUser
+}
+
+let foundUser = findUserById(78)
+console.log(foundUser)
